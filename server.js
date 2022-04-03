f69c0bf2d30344f8accebd8fc14e56eb68e63d5d
Jonathan <yonatan5789@gmail.com>
2022-04-03 15:17:33 +0300

    fffff

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session);
 app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.resolve(__dirname, 'public')));
+    app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
