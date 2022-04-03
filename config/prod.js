3822f5acf6bcab2f8dab043e2b9325ab1bd41c38
Jonathan <yonatan5789@gmail.com>
2022-04-03 14:52:59 +0300

    gggg

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': 'mongodb+srv://Roy:12345@cluster0.3bm1q.mongodb.net/toy_db?retryWrites=true&w=majority',
+  'dbURL': '',
 }
