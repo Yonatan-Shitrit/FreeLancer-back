c369b644a1f86baca73d5dee0a9d46270787f228
Yonatan-shitrit <jonatan.shitrit@gmail.com>
2023-01-06 12:26:05 +0200

    server target update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['https://freelancerapp.onrender.com','http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
     };
     app.use(cors(corsOptions));
